ccb560c6dd2ba20f88774116b39a08b4e07e08ba
olendvai <31893226+olendvai@users.noreply.github.com>
2017-11-17 20:39:03 +0100

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following program will cache the inverse of a given matrix and use it 
+## next time if available. Otherwise it will calculate and cache it.
 
-## Write a short comment describing this function
+## makeCacheMatrix returns a list of functions, which could be called in 
+## cacheSolve function either to give the inverse or store the calculated one.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <-matrix(nrow =nrow(x), ncol = nrow(x))
+  set <- function(y) {
+    x <<- y
+    i <<-matrix(nrow =nrow(x), ncol = nrow(x))
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Applying the functions of makeCacheMatrix, 
+## the following function will get or calculate and cache the inverse of the matrix. 
+## It is important to note that the argument is not the matrix itself,
+## but a variable, e.g. whatever <- makeCacheMatrix(our matrix).
+## cacheSolve(whatever) will give us the required result.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(all(!is.na(i))) {        # since not a single value should be examined, 
+                              #I used the function all() to check 
+                              # if all of the values are NAs
+    message("getting cached data")
+    return(i)
+  }
+  matrixdata <- x$get()
+  i <- solve (matrixdata)
+  x$setinverse(i)
+  i
 }
+
